152962c0012f2b0e4460d080f3ff3ce32f9e47e4
Ryuujimaru <wa.tinnarat_st@tni.ac.th>
2014-12-26 02:37:01 +0700

    Update Page
    หน้า Update แบบไม่มีลายเซน

--- app/Controller/RecordController.php
+++ app/Controller/RecordController.php
@@ -7,7 +7,7 @@ class RecordController extends AppController{
 	//set componant
 	public $components=array('Captcha', 'Session');	
 	//set object model
-	var $uses = array('Log','Department','Status');
+	var $uses = array('Log','Department','Status','Comment');
 			
 	public function index(){
 
@@ -144,7 +144,7 @@ class RecordController extends AppController{
 			if($this->request->data['Status']['status_name']==1)
 			{
 				$this->redirect(array(
-						'action' => 'Update'
+						'action' => 'Update',$_POST['log_id']
 				));
 			}
 			if($this->request->data['Status']['status_name']==2)
@@ -194,13 +194,92 @@ class RecordController extends AppController{
 		*/
 	}
 	
-	public function Update(){
+	public function Update($log_id = null){
 		
+ 		$log = $this->Log->find('all', array('conditions' => array('Log.log_id' => $log_id)));
+ 		
+ 		$this->set('log', $log[0]);
+
+		//debug($this->request->data);
+ 		//var_dump($log[0]);
+		//in case after fill form	
+
+ 		if($this->request->is('post')){
+			if($this->Captcha->validateCaptcha()) {
+				// OKの場合の処理 
+				
+				//debug what data is coming by POST
+				debug($this->request->data);
+
+				//set data
+				$emp_id = $this->request->data['Log']['Employee Id'];
+				$this->set('emp_id',$emp_id);
+								
+				$first_name = $this->request->data['Log']['first_name'];
+				$this->set('first_name',$first_name);
+								
+				$last_name = $this->request->data['Log']['last_name'];
+				$this->set('last_name',$last_name);
+								
+				$mail = $this->request->data['Log']['email'];
+				$this->set('email',$mail);
+				
+				$tel = $this->request->data['Log']['Tel'];
+				$this->set('tel',$tel);
+												
+				$department = $this->request->data['Log']['department'];
+				$this->set('department',$department);
+				
+				$user_category = $this->request->data['Log']['user_category'];
+				$this->set('user_category',$user_category);
+				
+				$service = $this->request->data['Log']['service'];
+				$this->set('service',$service);
+				
+				$location = $this->request->data['Log']['location'];
+				$this->set('location',$location);
+				
+				$title = $this->request->data['Log']['title'];
+				$this->set('title',$title);
+				
+				$detail = $this->request->data['Log']['Detail'];
+				$this->set('detail',$detail);
+
+				$comment= $this->request->data['Log']['Comment'];
+				$this->set('comment',$comment);
+				
+				//INSERT data to DB
+				$this->Log->create();
+				$this->Log->save(array(
+						'log_id' => $log_id,
+						'status_id' => '1',
+						'emp_id' => $emp_id,
+						'first_name' => $first_name,
+						'last_name' => $last_name,
+						'mail'=> $mail,
+						'tel' => $tel,
+						'department' => $department,
+						'user_category' => $user_category,
+						'service' => $service,
+						'location' => $location,
+						'title' => $title,
+						'detail' => $detail
+				));
+
+				$this->Comment->save(array(
+						'log_id' => $log_id,
+						'comment' => $comment
+				));
+										
+				$this->render('confirm_new_record');
+			}
+		}
+
 	}
 		
-	public function Detail(log_id = null){
+	/*public function Detail(log_id = null){
 		
-	}
+	}*/
 	
 	public function Comment(){
 		
